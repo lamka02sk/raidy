2ae8f0c6b7e5b65bae90569864e34c780d096d6b
lamka02sk <lamka02sk@gmail.com>
2017-06-13 20:20:55 +0200

    Initial commit. README will be added later.

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+// Include gulp
+let gulp = require('gulp');
+
+// Include plugins
+let concat = require('gulp-concat');
+let babel = require('gulp-babel');
+let uglify = require('gulp-uglify');
+let rename = require('gulp-rename');
+
+// Concatenate JS files, compile with babel and uglify
+gulp.task('scripts', function() {
+    return gulp.src(['src/*.js'])
+        .pipe(concat('raidy.js'))
+        .pipe(gulp.dest('dist/'))
+        .pipe(babel({
+            presets: [
+                ['env', { modules: false }]
+            ]
+        }))
+        .pipe(gulp.dest('dist/babel/'))
+        .pipe(rename('raidy.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/'));
+});
+
+// Automatize tasks
+gulp.task('watch', function() {
+    gulp.watch(['src/*.js'], ['scripts']);
+});
+
+// Default task
+gulp.task('default', ['scripts', 'watch']);
